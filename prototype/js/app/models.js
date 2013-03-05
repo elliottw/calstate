71c6e7e49137c1c105dc5c37c4da7363bfa0cf12
Greg Nicholas <greg.nicholas@gmail.com>
2013-03-04 22:26:31 -0500

    basic models created

--- prototype/js/app/models.js
+++ prototype/js/app/models.js
@@ -1,3 +1,94 @@
 (function(){
-	App.models = {};
+	var Semester = Backbone.Model.extend({
+		defaults: {
+			label: '',
+			order: 0
+		}
+	});
+	var Semesters = Backbone.Collection.extend({
+		model: Semester
+	});
+
+
+	var Course = Backbone.Model.extend({
+		defaults: {
+			code: '',
+			semestersAvailable: [],	// list of Semester instances
+			prereqs: []				// list of Course instances
+		}
+	});
+	var Courses = Backbone.Collection.extend({
+		model: Course
+	});
+
+	var Placement = Backbone.Model.extend({
+		defaults: {
+			course: null,	// Course model instance
+			semester: null,	// Semester model instance
+			status: '',
+			grade: ''
+		}
+	});
+
+	var PlannerSemester = Backbone.Model.extend({
+		defaults: {
+			semester: null,	// Semester model instance
+			placements: []	// list of Placement instances
+		}
+	});
+	var Planner = Backbone.Collection.extend({
+		model: PlannerSemester
+	});
+
+	var FixedRequirement = Backbone.Model.extend({
+		defaults: {
+			course: null,	// Course instance
+			label: ''
+		}
+	});
+	var ChooseRequirement = Backbone.Model.extend({
+		defaults: {
+			courses: [],	// list of Course instances
+			label: '',
+			count: 1
+		}
+	});
+
+	var MajorRequirements = Backbone.Collection.extend({
+		// collection of *Requirements
+	});
+
+	var CatalogFilter = Backbone.Model.extend({
+		// not sure how this will be implemented yet
+	});
+	var Catalog = Backbone.Model.extend({
+		defaults: {
+			courses: null,		// Courses collection instance
+			activeFilter: null	// CatalogFilter model
+		}
+	});
+
+	/*** exports ***/
+
+	App.models = {
+		Semester: Semester,
+		Course: Course,
+		Placement: Placement,
+		PlannerSemester: PlannerSemester,
+		FixedRequirement: FixedRequirement,
+		ChooseRequirement: ChooseRequirement,
+		CatalogFilter: CatalogFilter,
+		Catalog: Catalog
+	};
+
+	App.collections = {
+		Semesters: Semesters,
+		Courses: Courses,
+		Planner: Planner,
+		MajorRequirements: MajorRequirements
+	};
+
+	App.constants = {
+
+	};
 })();
