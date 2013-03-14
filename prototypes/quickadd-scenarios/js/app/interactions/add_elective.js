fc6b57b0ac98a2ebea49be7c8a33166e93cc411e
Greg Nicholas <greg.nicholas@gmail.com>
2013-03-14 14:45:48 -0400

    sidebar elective now showing course page

--- prototypes/quickadd-scenarios/js/app/interactions/add_elective.js
+++ prototypes/quickadd-scenarios/js/app/interactions/add_elective.js
@@ -2,7 +2,30 @@ App.module("Interactions", function(Interactions, App, Backbone, Marionette, $, 
     Interactions.AddElectiveController = Marionette.Controller.extend({
         initialize: function(options) {
             _.bindAll(this, 'forward');
-            this.forward();
+            this.showInfo();
+        },
+
+        showInfo: function() {
+            var view = new App.Catalog.CourseInfoView({
+                model: new Backbone.Model({
+                    course: slots.elecSlot3.get('course'),
+                    terms: [
+                        {label: 'Fall 2013', selected: false},
+                        {label: 'Spring 2014', selected: false},
+                        {label: 'Fall 2014', selected: false}
+                    ],
+                    satisfies: [
+                        {label: 'Block E', checked: true, fixed: true}
+                    ]
+                })
+            });
+
+            App.quickAddRegion.show(view);
+
+            var that = this;
+            view.on('courseSelected', function() {
+                that.forward();
+            });
         },
         forward: function() {
             // add course to sidebar
@@ -15,6 +38,8 @@ App.module("Interactions", function(Interactions, App, Backbone, Marionette, $, 
             views.fall.collection.add(new App.Planner.PlacedCourse({
                 course: course
             }));
+
+            App.quickAddRegion.close();
         }
     });
 });
--- prototypes/quickadd-scenarios/js/app/interactions/replace_elective.js
+++ prototypes/quickadd-scenarios/js/app/interactions/replace_elective.js
@@ -2,17 +2,44 @@ App.module("Interactions", function(Interactions, App, Backbone, Marionette, $, 
     Interactions.ReplaceElectiveController = Marionette.Controller.extend({
         initialize: function(options) {
             _.bindAll(this, 'forward');
-            this.forward();
+
+            this.newCourse = {code: 'ELEC 104'};
+            this.showInfo();
         },
+
+        showInfo: function() {
+            var view = new App.Catalog.CourseInfoView({
+                model: new Backbone.Model({
+                    course: this.newCourse,
+                    terms: [
+                        {label: 'Fall 2013', selected: false},
+                        {label: 'Spring 2014', selected: false},
+                        {label: 'Fall 2014', selected: false}
+                    ],
+                    satisfies: [
+                        {label: 'Block E', checked: true, fixed: true}
+                    ]
+                })
+            });
+
+            App.quickAddRegion.show(view);
+
+            var that = this;
+            view.on('courseSelected', function() {
+                that.forward();
+            });
+        },
+
         forward: function() {
+            App.quickAddRegion.close();
             // add course to sidebar
-            var newCourse = {code: 'ELEC 105'};
-            slots.elecSlot2.set('course', newCourse);
-            slots.elecSlot2.unset('interactionController');
+
+            slots.elecSlot1.set('course', this.newCourse);
+            slots.elecSlot1.unset('interactionController');
             views.req1.render();
 
             // add course to semester
-            placed.course2.set('course', newCourse);
+            placed.course1.set('course', this.newCourse);
             views.fall.render();
         }
     });
